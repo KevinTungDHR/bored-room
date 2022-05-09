178f2b208136b45b56e31cab592bbe864eccc1b9
seanodea <seanodea11@gmail.com>
2022-05-08 21:03:07 -0400

    state > errors > session slice added to state

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -4,7 +4,7 @@ import jwt_decode from 'jwt-decode';
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
-export const RECEIVE_USER_LOGIN = "RECEIVE_USER_LOGIN";
+export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
 
 export const logoutUser = () => ({
     type: RECEIVE_USER_LOGOUT
@@ -16,7 +16,7 @@ export const receiveCurrentUser = currentUser => ({
 });
 
 export const receiveUserSignIn = () => ({
-    type: RECEIVE_USER_LOGIN
+    type: RECEIVE_USER_SIGN_IN
 }); 
 
 export const receiveErrors = errors => ({
@@ -24,13 +24,26 @@ export const receiveErrors = errors => ({
     errors
 });
 
-// export const signup = user => dispatch => (
-//     APIUtil.signup(user).then(() => (
-//         dispatch(receiveUserSignIn())
-//     ), err => (
-//         dispatch(receiveErrors(err.response.data))
-//     ))
-// );
+export const signup = user => dispatch => (
+    APIUtil.signup(user).then(() => (
+        dispatch(receiveUserSignIn())
+    ), err => (
+        dispatch(receiveErrors(err.response.data))
+    ))
+);
+
+export const login = user => dispatch => (
+    APIUtil.login(user).then(res => {   
+        const { token } = res.data;
+        localStorage.setItem('jwtToken', token);
+        APIUtil.setAuthToken(token);
+        const decoded = jwt_decode(token);
+        dispatch(receiveCurrentUser(decoded))
+    })
+        .catch(err => {
+            dispatch(receiveErrors(err.response.data));
+        })
+)
 
 export const logout = () => dispatch => {
     // Remove the token from local storage
--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
-// import LoginFormContainer from './session/login_form_container';
-// import SignupFormContainer from './session/signup_form_container';
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
 import MainPage from './main/main_page';
 
 const App = () => (
@@ -11,8 +11,8 @@ const App = () => (
         <NavBarContainer />
         <Switch>
             <AuthRoute exact path="/" component={MainPage} />
-            {/* <AuthRoute exact path="/login" component={LoginFormContainer} /> */}
-            {/* <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
+            <AuthRoute exact path="/login" component={LoginFormContainer} />
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
         </Switch>
     </div>
 
