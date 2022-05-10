8211ebf33c9db189f77c41a4233e61954b7c719e
App Academy Student <email@email.com>
2022-05-10 16:05:23 -0400

    Got websockets working

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
+
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const rooms = require("./routes/api/rooms");
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const cors = require('cors');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());  
 app.use("/api/users", users);
@@ -20,4 +23,22 @@ app.use("/api/rooms", rooms);
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const server = app.listen(port, () => console.log(`Server is running on port ${port}`));
+const io = require('socket.io')(server, { cors: { origin: "*"}});
+
+io.on("connection", socket => {
+  console.log("New WS Connection");
+  console.log(socket.id);
+
+  socket.emit('message', "Connected to Backend");
+  socket.broadcast.emit('message', "User has connected");
+
+  socket.on('send_message', (data) => {
+    socket.broadcast.emit("receive_message", data);
+  })
+
+  socket.on("disconnect", () => {
+    io.emit("message", "user has left");
+    console.log("User Disconnected");
+  });
+});
--- frontend/src/components/rooms/room.jsx
+++ frontend/src/components/rooms/room.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+const socket = io();
 const Room = ({ roomCode }) => {
+  const [message, setMessage] = useState("");
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    return () => socket.disconnect();
+  }, []);
+
+  useEffect(()=> {
+      socket.on("message", (data) => console.log(data));
+      socket.on("receive_message", (data) => {
+        setList(list => [...list, data.message]);
+      });
+  },[socket]);
+
+  
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    socket.emit('send_message', { message: message });
+    console.log("test")
+  }
+
   return(
     <div>
       In Room {roomCode}
+      <input type="text" onChange={(e) => setMessage(e.target.value)}/>
+      <button onClick={sendMessage} >Send</button>
+      <ul>
+        {list.map((item, idx) => <li key={idx}>{item}</li>)}
+      </ul>
     </div>
   );
 }
