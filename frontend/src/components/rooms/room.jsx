a583297f43adfbb56c1a1cafea844ee9156f2b8f
App Academy Student <email@email.com>
2022-05-11 13:57:12 -0400

    Sit and get up from rooms

--- frontend/src/components/rooms/room.jsx
+++ frontend/src/components/rooms/room.jsx
@@ -2,15 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { io } from 'socket.io-client';
-import { fetchRoom } from '../../actions/room_actions';
+import { fetchRoom  } from '../../actions/room_actions';
+import { joinRoom, leaveRoom } from '../../util/rooms_util';
+import { receiveRoom } from '../../actions/room_actions';
 const socket = io();
 
 const Room = () => {
   const [message, setMessage] = useState("");
   const [list, setList] = useState([]);
-  const { code: roomCode } = useParams()
+  // const [joinFulfilled, setJoinFulfilled] = useState(true);
+  // const [leaveFulfilled, setLeaveFulfilled] = useState(true);
+
+  const { code: roomCode } = useParams();
   const dispatch = useDispatch();
-  const rooms = useSelector(state => state.entities.rooms)
+  const rooms = useSelector(state => state.entities.rooms);
+
   useEffect(() => {
     socket.emit("join_room", roomCode);
     dispatch(fetchRoom(roomCode));
@@ -22,6 +28,8 @@ const Room = () => {
       socket.on("receive_message", (data) => {
         setList(list => [...list, data.message]);
       });
+      socket.on("user_sits", (room) => dispatch(receiveRoom(room)));
+      socket.on("user_leaves", (room) => dispatch(receiveRoom(room)));
   },[socket]);
 
   const sendMessage = (e) => {
@@ -29,6 +37,16 @@ const Room = () => {
     socket.emit('send_message', { message: message, roomCode: roomCode });
   };
   
+  const joinSeat = (e) => {
+    console.log("Click before fulfill check");
+    e.preventDefault();
+    joinRoom(roomCode)
+  };
+
+  const leaveSeat = (e) => {
+    e.preventDefault();
+    leaveRoom(roomCode)
+  };
 
   return(
     <div>
@@ -43,6 +61,8 @@ const Room = () => {
       <ul>
         {Object.values(rooms).length > 0 && rooms[roomCode].seatedUsers.map((user, idx) => <li key={idx}>{user.handle}</li>)}
       </ul>
+      <button onClick={joinSeat}>Sit</button>
+      <button onClick={leaveSeat}>Get Up</button>
     </div>
   );
 }
