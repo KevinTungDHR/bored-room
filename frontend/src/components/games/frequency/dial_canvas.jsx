38920100f47dfad7b9e188fc5bd1c68694711200
kevin <kevinkevintung@gmail.com>
2022-05-25 21:35:01 -0400

    Fix dial function

--- frontend/src/components/games/frequency/dial_canvas.jsx
+++ frontend/src/components/games/frequency/dial_canvas.jsx
@@ -23,14 +23,17 @@ const DialCanvas = props => {
   const handleMouseMove = (e) => {
     if(isDragging){
       const canvas = canvasRef.current;
+      let rect = canvas.getBoundingClientRect()
 
-      let point = { x: e.clientX - canvas.offsetLeft - 315, y: e.clientY - canvas.offsetTop - 350}
+      let point = { x: e.clientX - rect.left - 315, 
+        y: e.clientY - rect.top - 350}
+
       setGuess(Math.floor(calculateDegrees(point)));
     }
   }
   const calculateDegrees = (point) => {
     const b = {x: 0, y: 0}
-    const c = {x: -300, y: 0}
+    const c = {x: -315, y: 0}
     let ab = Math.sqrt(Math.pow(b.x - point.x, 2) + Math.pow(b.y - point.y, 2)) 
     let bc = Math.sqrt(Math.pow(b.x - c.x, 2) + Math.pow(b.y - c.y, 2)) 
     let ac = Math.sqrt(Math.pow(c.x - point.x, 2) + Math.pow(c.y - point.y, 2)) 
