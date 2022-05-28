0d519e7bc2efc49e1b53206ec3d55ba17a9071f5
seanodea <seanodea11@gmail.com>
2022-05-27 20:27:32 -0400

    about changes

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -42,7 +42,7 @@ const AboutDevelopers = () => {
                     <img className='headshot' src={sean} />
                     <span className='name'>Sean O'Dea</span>
                     <span className='role'>Frontend Lead</span>
-                    <p className='bio-description'></p>
+                    <p className='bio-description'>Sea </p>
                     <div className='connect-links'>
                         <span>Connect: </span>
                         <a href='https://github.com/sodea1'><img src={github} /></a>
