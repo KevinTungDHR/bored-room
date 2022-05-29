86092031d2a8c90521ace101472c691fb53d1854
seanodea <seanodea11@gmail.com>
2022-05-29 13:25:30 -0400

    end game conditional

--- frontend/src/components/taking_six/game_component.jsx
+++ frontend/src/components/taking_six/game_component.jsx
@@ -128,14 +128,14 @@ const GameComponent = ({ roomCode, socket }) => {
           </div>
 
           <div className='game-background'>
-            {/* {gameState.actions[0] === 'gameEnd' &&  */}
+            {gameState.actions[0] === 'gameEnd' &&
             <motion.div
               className='end-game-backdrop'
               animate={{ scale: [0, 1] }}
               transition={{ duration: 0.5 }}>
               <GameEnd allUsers={allUsers} allPlayers={allPlayers} />
             </motion.div>
-            {/* } */}
+            }
             <div className='game-container'>
 
               <div className='board-container'>
