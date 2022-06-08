af6b9bd503fff0290ddaf5a1356a12b7283c6a69
kevin <kevinkevintung@gmail.com>
2022-06-08 17:28:06 -0400

    Added 12 additional profile icons

--- frontend/src/components/profile/avatar.jsx
+++ frontend/src/components/profile/avatar.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import yoda from '../../assets/images/yoda.png';
 import monkey from '../../assets/images/coolMonkey.png';
 import socrates from '../../assets/images/socrates.png';
+import reindeer from '../../assets/images/reindeer.png';
+import controller from '../../assets/images/controller.png';
+import gameboy from '../../assets/images/gameboy.png';
+import owl from '../../assets/images/owl.png';
+import pirahna from '../../assets/images/pirahna.png';
+import robot from '../../assets/images/robot.png';
+import snowman from '../../assets/images/snowman.png';
+import camera from '../../assets/images/camera.png';
+import coffee from '../../assets/images/coffee.png';
+import redRobot from '../../assets/images/redRobot.png';
+import pumpkin from '../../assets/images/pumpkin.png';
+import rocket from '../../assets/images/rocket.png';
 
 class Avatar extends React.Component {
     constructor(props) {
@@ -33,6 +45,19 @@ class Avatar extends React.Component {
                     <button onClick={this.handleClick} value="monkey" className='profile-avatar-modal' style={{ backgroundImage: "url(" + monkey + ")" }}></button>
                     <button onClick={this.handleClick} value="yoda" className='profile-avatar-modal' style={{ backgroundImage: "url(" + yoda + ")" }}></button>
                     <button onClick={this.handleClick} value="socrates" className='profile-avatar-modal' style={{ backgroundImage: "url(" + socrates + ")" }}></button>
+                    <button onClick={this.handleClick} value="reindeer" className='profile-avatar-modal' style={{ backgroundImage: "url(" + reindeer + ")" }}></button>
+                    <button onClick={this.handleClick} value="controller" className='profile-avatar-modal' style={{ backgroundImage: "url(" + controller + ")" }}></button>
+                    <button onClick={this.handleClick} value="gameboy" className='profile-avatar-modal' style={{ backgroundImage: "url(" + gameboy + ")" }}></button>
+                    <button onClick={this.handleClick} value="owl" className='profile-avatar-modal' style={{ backgroundImage: "url(" + owl + ")" }}></button>
+                    <button onClick={this.handleClick} value="pirahna" className='profile-avatar-modal' style={{ backgroundImage: "url(" + pirahna + ")" }}></button>
+                    <button onClick={this.handleClick} value="robot" className='profile-avatar-modal' style={{ backgroundImage: "url(" + robot + ")" }}></button>
+                    <button onClick={this.handleClick} value="snowman" className='profile-avatar-modal' style={{ backgroundImage: "url(" + snowman + ")" }}></button>
+                    <button onClick={this.handleClick} value="camera" className='profile-avatar-modal' style={{ backgroundImage: "url(" + camera + ")" }}></button>
+                    <button onClick={this.handleClick} value="coffee" className='profile-avatar-modal' style={{ backgroundImage: "url(" + coffee + ")" }}></button>
+                    <button onClick={this.handleClick} value="redRobot" className='profile-avatar-modal' style={{ backgroundImage: "url(" + redRobot + ")" }}></button>
+                    <button onClick={this.handleClick} value="pumpkin" className='profile-avatar-modal' style={{ backgroundImage: "url(" + pumpkin + ")" }}></button>
+                    <button onClick={this.handleClick} value="rocket" className='profile-avatar-modal' style={{ backgroundImage: "url(" + rocket + ")" }}></button>
+
                 </div>
             </div>
         )
--- frontend/src/components/profile/user_card.jsx
+++ frontend/src/components/profile/user_card.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import yoda from '../../assets/images/yoda.png';
 import monkey from '../../assets/images/coolMonkey.png';
 import socrates from '../../assets/images/socrates.png';
+import reindeer from '../../assets/images/reindeer.png';
+import controller from '../../assets/images/controller.png';
+import gameboy from '../../assets/images/gameboy.png';
+import owl from '../../assets/images/owl.png';
+import pirahna from '../../assets/images/pirahna.png';
+import robot from '../../assets/images/robot.png';
+import snowman from '../../assets/images/snowman.png';
+import camera from '../../assets/images/camera.png';
+import coffee from '../../assets/images/coffee.png';
+import redRobot from '../../assets/images/redRobot.png';
+import pumpkin from '../../assets/images/pumpkin.png';
+import rocket from '../../assets/images/rocket.png';
 import user_prof from '../../assets/images/user_prof.png';
 import { NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
@@ -24,9 +36,20 @@ const UserCard = ({user, status}) => {
     'noimage': user_prof,
     'yoda': yoda,
     'monkey': monkey,
-    'socrates': socrates
-  };
-
+    'socrates': socrates,
+    'reindeer': reindeer,
+    'controller': controller,
+    'gameboy': gameboy,
+    'owl': owl,
+    'pirahna': pirahna,
+    'robot': robot,
+    'snowman': snowman,
+    'camera': camera,
+    'coffee': coffee,
+    'redRobot': redRobot,
+    'pumpkin': pumpkin,
+    'rocket': rocket
+};
   const handleUnblock = (e) => {
     e.preventDefault();
     e.stopPropagation();
